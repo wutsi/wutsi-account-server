d91f324f877d842fd65cf4169cb2fd9078607f2d
wutsi <herve.tchepannou@wutsi.com>
2022-05-20 11:10:57 -0400

    update api

--- src/main/kotlin/com/wutsi/platform/account/dto/SearchAccountRequest.kt
+++ src/main/kotlin/com/wutsi/platform/account/dto/SearchAccountRequest.kt
@@ -1,11 +1,17 @@
 package com.wutsi.platform.account.dto
 
-data class SearchAccountRequest(
-    val ids: List<Long> = emptyList(),
-    val phoneNumber: String? = null,
-    val business: Boolean? = null,
-    val hasStore: Boolean? = null,
-    val limit: Int = 30,
-    val offset: Int = 0,
-    val sortBy: String? = null
+import kotlin.Boolean
+import kotlin.Int
+import kotlin.Long
+import kotlin.String
+import kotlin.collections.List
+
+public data class SearchAccountRequest(
+    public val ids: List<Long> = emptyList(),
+    public val phoneNumber: String? = null,
+    public val business: Boolean? = null,
+    public val hasStore: Boolean? = null,
+    public val limit: Int = 30,
+    public val offset: Int = 0,
+    public val sortBy: String? = null
 )
