dea2c9b72d84820c9b91a5ec3a3493f4d38115ed
wutsi <herve.tchepannou@wutsi.com>
2022-07-29 17:16:54 -0400

    add API key

--- src/main/kotlin/com/wutsi/platform/account/dto/PaymentMethodSummary.kt
+++ src/main/kotlin/com/wutsi/platform/account/dto/PaymentMethodSummary.kt
@@ -1,18 +1,15 @@
 package com.wutsi.platform.account.dto
 
-import org.springframework.format.`annotation`.DateTimeFormat
+import org.springframework.format.annotation.DateTimeFormat
 import java.time.OffsetDateTime
-import kotlin.String
 
-public data class PaymentMethodSummary(
-    public val token: String = "",
-    public val type: String = "",
-    public val provider: String = "",
-    public val ownerName: String = "",
-    public val maskedNumber: String = "",
-    public val phone: Phone = Phone(),
-    @get:DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
-    public val created: OffsetDateTime = OffsetDateTime.now(),
-    @get:DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ")
-    public val updated: OffsetDateTime = OffsetDateTime.now(),
+data class PaymentMethodSummary(
+    val token: String = "",
+    val type: String = "",
+    val provider: String = "",
+    val ownerName: String = "",
+    val maskedNumber: String = "",
+    val phone: Phone? = null,
+    @get:DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ") val created: OffsetDateTime = OffsetDateTime.now(),
+    @get:DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ssZ") val updated: OffsetDateTime = OffsetDateTime.now(),
 )
